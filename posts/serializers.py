50aa7cf0047369709b32ad9b5b15a00643e7704f
LinnSilver <baralinn@gmail.com>
2023-07-13 16:03:11 +0000

    Add img and url feald in post serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -7,6 +7,8 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    image = serializers.ImageField(upload_to='post_images/', required=False)
+    url = serializers.URLField(required=False)
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
